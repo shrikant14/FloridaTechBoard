4333d6a4b7913a943d9eafa9289162327365359f
shrikant <shrikant.pm14@gmail>
2016-04-12 18:36:12 -0400

    Created Launch activity without logic

--- app/src/main/java/in/floridatechboard/myboard/Activities/LaunchActivity.java
+++ app/src/main/java/in/floridatechboard/myboard/Activities/LaunchActivity.java
@@ -1,54 +1,45 @@
 package in.floridatechboard.myboard.Activities;
 
+import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.TextView;
 
 import in.floridatechboard.myboard.R;
 
 public class LaunchActivity extends AppCompatActivity {
 
+    TextView title,subTitle;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_launch);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        title=(TextView)findViewById(R.id.textViewTitle);
+        subTitle=(TextView)findViewById(R.id.textViewSubTitle);
+
+        String fontPath = "fonts/Caviar_Dreams_Bold.ttf";
+        Typeface tf = Typeface.createFromAsset(getAssets(), fontPath);
+        title.setTypeface(tf);
+        title.setText("FLORIDA TECH");
+
+        subTitle.setTypeface(tf);
+        subTitle.setText("BOARD");
+        addDelay();
+    }
+
+    public void addDelay(){
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void run() {
+                // Do something after 5s = 5000ms
+                Intent in = new Intent(LaunchActivity.this, HomeActivity.class);
+                in.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(in);
             }
-        });
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_launch, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+        }, 10000);
     }
 }
